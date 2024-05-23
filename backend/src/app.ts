d5fd24135242d0fadce08ce126915166c23a03db
Hafiz Ahmed <hafizahmed.tx@gmail.com>
2024-05-23 15:41:41 -0500

    ref: removed next js boilerplate

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,9 @@ import cors from "cors";
 
 import UserRouter from "./user/routes";
 import AuthRouter from "./auth/routes";
+import WatchlistRouter from "./watchlist/routes/watchlist.routes";
+import StockRouter from "./stocks/routes";
+
 import { errorHandler } from "./middlewares/error.handler";
 import { notFound } from "./middlewares/not.found";
 
@@ -16,7 +19,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", UserRouter);
+app.use("/api/watchlist", WatchlistRouter);
+app.use("/api/stocks", StockRouter);
 app.use("/auth", AuthRouter);
+
 
 app.use(notFound);
 app.use(errorHandler);
