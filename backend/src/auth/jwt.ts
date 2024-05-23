b63ce574bf2ffcd91d0b1af12c996c9304223625
Hafiz Ahmed <hafizahmed.tx@gmail.com>
2024-05-23 12:43:06 -0500

    update: added jwt helper methods

---
+++ backend/src/auth/jwt.ts
@@ -0,0 +1,28 @@
+import { JWT_SECRET_KEY } from "../constants/environment.variables";
+import jwt from "jsonwebtoken";
+import { isError } from "../utils/type.guards";
+import { UserDto } from "../user/dto/user";
+
+export function generateAccessToken(email: string) {
+  const payload = {
+    email: email,
+  };
+
+  const secret = JWT_SECRET_KEY;
+  const options = { expiresIn: "1h" };
+
+  return jwt.sign(payload, secret, options);
+}
+
+export function verifyAccessToken(token: string) {
+  const secret = JWT_SECRET_KEY;
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    return { success: true, data: decoded };
+  } catch (error) {
+    if (isError(error)) return { success: false, error: error.message };
+
+    return { success: false, error: error };
+  }
+}
